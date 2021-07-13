0d7d67c5eecdbed06d2ddf92b3f0e112a4c9ac40
Arūnas Dovainis <arunas.dovainis@gmail.com>
2021-07-13 12:29:44 +0300

    Pirma paskaita

---
+++ js/components/Dictionary.js
@@ -0,0 +1,9 @@
+class Dictionary {
+    constructor(selectorius, pavadinimas, list) {
+        this.selector = selectorius;
+        this.name = pavadinimas;
+        this.dictionarylist = list;
+    }
+}
+
+export { Dictionary }
